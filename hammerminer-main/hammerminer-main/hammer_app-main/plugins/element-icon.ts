d2c90e544c512f67174ec08c18ecca675a6cac4d
qq <75964930+formatqq@users.noreply.github.com>
2026-03-01 10:06:43 +0800

    Delete hammerminer-main/hammerminer-main directory

--- hammerminer-main/hammerminer-main/hammer_app-main/plugins/element-icon.ts
+++
@@ -1,6 +0,0 @@
-import * as ElementPlusIconsVue from '@element-plus/icons-vue'
-export default defineNuxtPlugin(({ vueApp }) => {
-  for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-    vueApp.component(key, component)
-  }
-})
--- hammerminer-main/hammerminer-main/hammer_app-main/plugins/vue-gtag.client.js
+++
@@ -1,9 +0,0 @@
-import VueGtag, { trackRouter } from 'vue-gtag-next'
-export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.use(VueGtag, {
-    property: {
-      id: 'UA-256171990-1'
-    }
-  })
-  trackRouter(useRouter())
-})
