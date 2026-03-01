ca1c49881444d42cfcded3a93a9a41296df43b01
root <root@localhost.localdomain>
2026-03-01 09:48:46 +0800

    提交信息

---
+++ hammer_app-main/pages/index.vue
@@ -0,0 +1,375 @@
+<template>
+  <div class="common-container">
+    <swiper
+      :active-index="activeIndex"
+      :modules="modules"
+      :slides-per-view="1"
+      :space-between="50"
+      :autoplay="{ delay: 5000 }"
+      :pagination="{ clickable: true }"
+      @slideChange="slideChange"
+    >
+      <swiper-slide>
+        <section
+          class="banner"
+          id="banner01"
+        >
+        </section>
+      </swiper-slide>
+       <swiper-slide>
+        <section
+          class="banner"
+          id="banner04"
+        >
+        </section>
+      </swiper-slide>
+      
+      <swiper-slide>
+        <section
+          class="banner"
+          id="banner03"
+        >
+        </section>
+      </swiper-slide>
+
+      <swiper-slide>
+        <section
+          class="banner"
+          id="banner02"
+        >
+        </section>
+      </swiper-slide>
+    </swiper>
+    <section class="params-seciton">
+      <div class="title">
+        <div>{{ $t('home.params') }}</div>
+        <img src="/img/sub-line.png" />
+      </div>
+      <div class="content d-flex">
+        <div
+          v-for="item of paramsList"
+          class="params-box"
+        >
+          <img
+            class="describe mb10"
+            :src="`/img/params${item.id}.png`"
+          />
+          <div
+            class="describe-title"
+            v-html="$t(`home.product${activeIndex}.params${item.id}_title`)"
+          />
+          <div class="params">
+            <div v-for="params of item.paramsNum">
+              {{ $t(`home.product${activeIndex}.params${item.id}_${params}`) }}
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="shop-section">
+      <div class="title">
+        <div>{{ $t('home.shop') }}</div>
+        <img src="/img/sub-line.png" />
+      </div>
+      <div class="content d-flex-between">
+        <a
+          v-for="item of shopsList"
+          :href="item.link"
+          target="_blank"
+        >
+          <img
+            :src="item.image || `/img/${item.name}.png`"
+          />
+        </a>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { Pagination, A11y, Autoplay } from 'swiper'
+
+// Import Swiper Vue.js components
+import { Swiper, SwiperSlide } from 'swiper/vue'
+// Import Swiper styles
+import 'swiper/css'
+import 'swiper/css/autoplay'
+import 'swiper/css/pagination'
+
+const modules = [Pagination, A11y, Autoplay]
+
+const activeIndex = ref(0)
+const slideChange = (swiper: any) => {
+  activeIndex.value = swiper.activeIndex
+}
+
+const paramsList = computed(() => {
+  switch (activeIndex.value) {
+    case 0:
+      return [
+        {
+          id: '01',
+          paramsNum: 3,
+        },
+        {
+          id: '02',
+          paramsNum: 8,
+        },
+        {
+          id: '03',
+          paramsNum: 5,
+        },
+      ]
+    case 1:
+      return [
+        {
+          id: '01',
+          paramsNum: 5,
+        },
+        {
+          id: '02',
+          paramsNum: 3,
+        },
+        {
+          id: '03',
+          paramsNum: 6,
+        },
+        {
+          id: '04',
+          paramsNum: 3,
+        },
+      ]
+    case 2:
+      return [
+        {
+          id: '01',
+          paramsNum: 7,
+        },
+        {
+          id: '02',
+          paramsNum: 3,
+        },
+        {
+          id: '03',
+          paramsNum: 6,
+        },
+        {
+          id: '04',
+          paramsNum: 3,
+        },
+      ]     
+    default:
+      return [
+        {
+          id: '01',
+          paramsNum: 5,
+        },
+        {
+          id: '02',
+          paramsNum: 3,
+        },
+        {
+          id: '03',
+          paramsNum: 6,
+        },
+        {
+          id: '04',
+          paramsNum: 3,
+        },
+      ]
+  }
+})
+
+const shopsList = ref([
+  {
+    name: 'amazon',
+    link: '//www.amazon.com/dp/B0BHNQ3GM6',
+  },
+  {
+    name: 'ebay',
+    link: '//www.ebay.com/itm/354136904551',
+  },
+  {
+    name: 'plebSource',
+    link: '//onestopmining.shop/',
+    image: '/img/plebSource.png',
+  },
+])
+
+</script>
+
+<style lang="less" scoped>
+section {
+  width: 1080px;
+  margin: 0 auto 50px;
+
+  .title {
+    width: fit-content;
+    font-size: 24px;
+    margin-bottom: 40px;
+
+    img {
+      width: 100%;
+      max-width: 300px;
+      height: 8px;
+    }
+  }
+}
+
+.banner {
+  width: 100vw;
+  height: 36vw;
+  color: #fff;
+
+  &#banner01 {
+    background: #1b1b1b url('/img/banner-1.jpg');
+    background-size: cover;
+  }
+  
+ &#banner04 {
+    background: url('/img/banner4-bg.png');
+    background-size: cover;
+  }
+  
+  &#banner03 {
+    background: url('/img/banner3-bg.png');
+    background-size: cover;
+  }
+
+  &#banner02 {
+    background: url('/img/banner2-bg.png');
+    background-size: cover;
+  }
+
+  .miner {
+    width: 20vw;
+  }
+
+  .num {
+    font-family: 'Roboto';
+  }
+
+  h2 {
+    font-size: 4vw;
+    font-weight: normal;
+  }
+
+  h3 {
+    font-size: 2vw;
+    font-weight: normal;
+  }
+}
+
+.params-seciton {
+  .content {
+    flex-wrap: wrap;
+    border: 1px solid @main-color;
+
+    .params-box {
+      flex: 1;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      padding: 20px;
+
+      .title {
+        margin-bottom: 10px;
+      }
+
+      .describe {
+        width: 25%;
+      }
+
+      .describe-title {
+        font-size: 20px;
+        margin-bottom: 10px;
+        text-align: center;
+        color: rgba(83, 86, 87);
+        font-weight: bold;
+      }
+
+      &:nth-child(even) {
+        background-color: @main-color;
+        color: #fff;
+
+        .describe-title {
+          color: #fff;
+        }
+      }
+
+      .params {
+        width: 100%;
+        text-align: left;
+      }
+    }
+  }
+}
+
+.shop-section {
+  .content {
+    width: 60%;
+    margin: 0 auto;
+    gap: 20px;
+    align-items: center;
+
+    img {
+      max-height: 100px;
+      max-width: 100%;
+      height: auto;
+      width: auto;
+      object-fit: contain;
+    }
+  }
+}
+
+@media (max-width: 1024px) {
+  .common-container {
+    margin: 60px 0;
+  }
+
+  section {
+    width: 100%;
+    padding: 0 20px;
+    box-sizing: border-box;
+
+    .title {
+      font-size: 18x;
+    }
+  }
+
+  .banner {
+    &#banner02 {
+      .content {
+        width: 80%;
+
+        .split-line {
+          width: 120px;
+          margin: 10px 0;
+        }
+
+        span {
+          font-size: 12px;
+        }
+      }
+    }
+  }
+
+  .params-seciton {
+    .content {
+      .params-box {
+        box-sizing: border-box;
+        flex: auto;
+        width: 100%;
+      }
+    }
+  }
+
+  .shop-section {
+    .content {
+      width: 100%;
+      margin: 0 auto;
+    }
+  }
+}
+</style>
---
+++ hammer_app-main/pages/products/index.vue
@@ -0,0 +1,322 @@
+<template>
+  <div class="common-container">
+    <section class="banner">
+      <div class="content d-flex-between">
+      </div>
+    </section>
+    <section class="wide-section">
+      <div class="content detail-only wide-detail">
+        <img class="detail-image" :src="detail1Image" alt="detail-1">
+      </div>
+    </section>
+    <section class="wide-section">
+      <div class="title">{{ $t('products.modes') }}</div>
+      <div class="content detail-only wide-detail">
+        <img class="detail-image" :src="detail2Image" alt="detail-2">
+      </div>
+    </section>
+    <section class="wide-section">
+      <div class="title">{{ $t('products.roi') }}</div>
+      <div class="content detail-only wide-detail">
+        <img class="detail-image" :src="detail3Image" alt="detail-3">
+      </div>
+    </section>
+    <section class="wide-section">
+      <div class="title">{{ $t('products.service') }}</div>
+      <div class="content detail-only wide-detail">
+        <img class="detail-image" :src="detail4Image" alt="detail-4">
+      </div>
+    </section>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const locale = useCookie<string>('locale')
+
+const detail1Image = computed(() => {
+  const lang = locale.value || 'en'
+  return `/img/products/${lang}/detail-1.jpg`
+})
+
+const detail2Image = computed(() => {
+  const lang = locale.value || 'en'
+  return `/img/products/${lang}/detail-2.jpg`
+})
+
+const detail3Image = computed(() => {
+  const lang = locale.value || 'en'
+  return `/img/products/${lang}/detail-3.jpg`
+})
+
+const detail4Image = computed(() => {
+  const lang = locale.value || 'en'
+  return `/img/products/${lang}/detail-4.jpg`
+})
+</script>
+
+<style lang="less" scoped>
+section {
+  width: 1080px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin: 0 auto 20px;
+  font-weight: bold;
+
+  &.wide-section {
+    width: 1650px;
+    max-width: calc(100vw - 40px);
+  }
+
+  .title {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 500px;
+    height: 40px;
+    margin-bottom: 0;
+    text-align: center;
+    color: #fff;
+    font-size: 26px;
+
+    &.no-margin {
+      margin-bottom: 10px;
+    }
+  }
+
+  .sub-title {
+    font-size: 26px;
+    color: @main-color;
+    margin-bottom: 10px;
+  }
+
+  .content {
+    width: 100%;
+    height: 400px;
+
+    &.detail-only {
+      width: 100%;
+      height: auto;
+
+      .detail-image {
+        width: 100%;
+        display: block;
+      }
+    }
+
+    &.wide-detail {
+      width: 1650px;
+      max-width: 100%;
+    }
+
+    .miner-box {
+      width: 60%;
+      height: 100%;
+      background-color: rgb(244, 244, 244);
+      box-shadow: -12px 12px 0px @main-color;
+
+      img {
+        width: 80%;
+      }
+    }
+
+    .data-box {
+      width: 40%;
+      height: 100%;
+      border: 2px solid @main-color;
+      border-left: none;
+
+      .power {
+        display: block;
+        width: 80%;
+        margin: 0 auto;
+        font-size: 24px;
+      }
+    }
+
+    &.reverse {
+      flex-direction: row-reverse;
+
+      .miner-box {
+        box-shadow: 12px 12px 0px @main-color;
+      }
+
+      .data-box {
+        border-left: 2px solid @main-color;
+        border-right: none;
+      }
+    }
+
+    .mode-box {
+      width: 100%;
+      height: 80%;
+      position: relative;
+
+      .mode-cycle {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+        width: 180px;
+        height: 180px;
+
+        &.low {
+          position: absolute;
+          top: 10px;
+          left: 10px;
+          background: url('/img/low-mode.png');
+          background-position: center;
+          background-size: cover;
+        }
+
+        &.high {
+          position: absolute;
+          right: 10px;
+          bottom: 10px;
+          background: url('/img/high-mode.png');
+          background-position: center;
+          background-size: cover;
+        }
+      }
+    }
+
+    .warn {
+      color: red;
+      text-align: center;
+      margin: 10px 5px 0;
+    }
+
+    .payback-box {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+
+      div {
+        font-size: 20px;
+      }
+
+      span {
+        font-size: 36px;
+        color: @main-color;
+      }
+
+      img {
+        width: 70%;
+        margin: 20px 0;
+      }
+    }
+  }
+
+  .address {
+    width: 100%;
+    position: relative;
+
+    img {
+      width: 100%;
+    }
+  }
+
+  .tips {
+    width: 100%;
+    box-sizing: border-box;
+    position: absolute;
+    bottom: 0;
+    padding: 10px;
+    background: rgba(#fff, 0.3)
+  }
+}
+
+.banner {
+  width: 100vw;
+  height: 36vw;
+  margin-bottom: 60px;
+  color: #fff;
+  background: url('/img/banner-2.jpg');
+  background-size: cover;
+
+  .content {
+    height: 100%;
+    padding: 20px;
+    box-sizing: border-box;
+
+  }
+}
+
+@media (max-width: 1024px) {
+  .common-container {
+    margin: 60px 0;
+  }
+
+  section {
+    width: 100%;
+    padding: 0 20px;
+    box-sizing: border-box;
+    font-size: 12px;
+
+    .title {
+      width: 300px;
+      font-size: 20px;
+    }
+
+    .sub-title {
+      font-size: 20px;
+    }
+
+    .content {
+      height: 300px;
+
+      &.detail-only {
+        height: auto;
+      }
+
+      .miner-box {
+        width: 50%;
+        box-shadow: -5px 5px 0px @main-color;
+      }
+
+      &.reverse {
+        .miner-box {
+          box-shadow: 5px 5px 0px @main-color;
+        }
+      }
+
+      .data-box {
+        width: 50%;
+      }
+
+      .mode-box {
+        height: 78%;
+
+        .mode-cycle {
+          width: 110px;
+          height: 110px;
+        }
+      }
+
+      .warn {
+        margin: 0px 2px;
+      }
+
+      .payback-box {
+        div {
+          text-align: center;
+          font-size: 13px;
+        }
+
+        span {
+          font-size: 16px;
+        }
+
+        img {
+          width: 80%;
+        }
+      }
+    }
+
+    .tips {
+      width: 100%;
+    }
+  }
+}</style>
