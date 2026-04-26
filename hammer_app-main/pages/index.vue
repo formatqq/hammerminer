f6691ea001aa3ec7193ae5a881618cd4cbc8a2f2
qq <75964930+formatqq@users.noreply.github.com>
2026-04-26 19:17:48 +0800

    Update index.vue

--- hammer_app-main/pages/index.vue
+++ hammer_app-main/pages/index.vue
@@ -11,13 +11,7 @@
     >
 
 
-     <swiper-slide>
-        <section
-          class="banner"
-          id="banner00"
-        >
-        </section>
-      </swiper-slide>
+    
       
       
        <swiper-slide>
@@ -330,10 +324,7 @@ section {
   width: 100vw;
   height: 36vw;
   color: #fff;
-   &#banner00 {
-    background: #1b1b1b url('/img/banner1-4.jpg');
-    background-size: cover;
-  }
+
   
    &#banner01 {
     background: #1b1b1b url('/img/banner-1.jpg');
